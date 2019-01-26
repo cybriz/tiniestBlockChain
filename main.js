c223f342b02cc478f5b03fd805c065072fce8909
wong yew loke <wongyewloke@gmail.com>
2019-01-26 18:05:20 +0800

    added Proof Of Work

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const SHA256 = require("crypto-js/sha256")
+const SHA256 = require("crypto-js/sha256");
 
 class Block {
     constructor(index, timestamp, data, previousHash = '') {
@@ -7,16 +7,26 @@ class Block {
         this.data = data;
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
+        this.nonce = 0;
     }
 
     calculateHash(){
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+    }
+
+    mineBlock(difficulty){
+        while(this.hash.substring(0,difficulty) !== Array(difficulty+1).join("0")){
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+        console.log("block mined:" + this.hash)
     }
 }
 
 class Blockchain {
     constructor(){
         this.chain = [this.createGenesisBlock()];
+        this.difficulty = 3;
     }
 
     createGenesisBlock(){
@@ -29,8 +39,8 @@ class Blockchain {
 
     addBlock(newBlock){
         newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
-        console.log("hash: ", newBlock.hash = newBlock.calculateHash())
+        // newBlock.hash = newBlock.calculateHash();
+        newBlock.mineBlock(this.difficulty);
         this.chain.push(newBlock);
     }
 
@@ -40,28 +50,29 @@ class Blockchain {
             const previousBlock = this.chain[i - 1];
 
             if(currentBlock.hash !== currentBlock.calculateHash()) {
-                console.log("hi")
                 return false;
             }
 
             if(currentBlock.previousHash !== previousBlock.hash){
-                console.log("hey")
                 return false;
             }
         }
-        console.log("hello")
         return true;
     }
 }
 
 let sampleCoin = new Blockchain();
-sampleCoin.addBlock(new Block(1, '26/01/2019', { amount: 4 }));
-sampleCoin.addBlock(new Block(2, '27/02/2019', { amount: 10 }));
+
+console.log("mining block 1...")
+sampleCoin.addBlock(new Block(1, '26/01/2019', { amount: 5 }));
+
+console.log("mining block 2...")
+sampleCoin.addBlock(new Block(2, '27/02/2019', { amount: 25 }));
 
 // console.log(JSON.stringify(sampleCoin, null , 4))
-console.log('is blockchain valid? ', sampleCoin.isChainValid());
+// console.log('is blockchain valid? ', sampleCoin.isChainValid());
 
-sampleCoin.chain[1].data = { amount: 100 };
-sampleCoin.hash = sampleCoin.chain[1].calculateHash();
+// sampleCoin.chain[1].data = { amount: 100 };
+// sampleCoin.hash = sampleCoin.chain[1].calculateHash();
 
-console.log('is blockchain valid? ', sampleCoin.isChainValid());
+// console.log('is blockchain valid? ', sampleCoin.isChainValid());
